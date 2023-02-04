a4cf25efe8d8fd821378b585e6554075ed1b432d
darwin242209 <115344849+darwin242209@users.noreply.github.com>
2023-02-04 23:18:16 +0800

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,6 +1,6 @@
 class Config:
     BOT_USE = False
     BOT_TOKEN = '6036016901:AAHRwWJ75lxFThjVgzqOqNysOWTZq2ISyqk'                         # from @botfather
-    APP_ID = 5293972                              # from https://my.telegram.org/apps
+    API_ID = 5293972                              # from https://my.telegram.org/apps
     API_HASH = 'fc7377be14fb556b2fe43945e1910e51'                          # from https://my.telegram.org/apps
     AUTH_USERS = [1744065403]
